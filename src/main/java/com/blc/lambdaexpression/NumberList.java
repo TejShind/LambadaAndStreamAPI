1d7c96b8d0eac1dfe121c4a955b22262e8c146d0
TejShind <tej.gholap25@gmail.com>
2022-04-03 17:29:20 +0530

    [Tejaswini]ADDED:Feature for changes in code

--- src/main/java/com/blc/lambdaexpression/NumberList.java
+++ src/main/java/com/blc/lambdaexpression/NumberList.java
@@ -1,15 +1,12 @@
 package com.blc.lambdaexpression;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class NumberList {
     public static void main(String[] args) {
         List<Integer> myList = Arrays.asList(1, 6, 9, 44);
-
 
         Function<Integer, Double> doubleFunction = (n) -> n.doubleValue();
         myList.forEach(value -> System.out.println(" Integer Value to Double : " + doubleFunction.apply(value)));
