555a07172e964e67807885836d6f3b242ad48f0c
TejShind <tej.gholap25@gmail.com>
2022-04-03 17:21:58 +0530

    [Tejaswini]ADDED:Feature for convert Interger value to Double value by using Functional Interface

--- src/main/java/com/blc/lambdaexpression/NumberList.java
+++ src/main/java/com/blc/lambdaexpression/NumberList.java
@@ -4,30 +4,15 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberList {
     public static void main(String[] args) {
         List<Integer> myList = Arrays.asList(1, 6, 9, 44);
-        ///By using Iterator
-//        Iterator<Integer> iterator = myList.iterator();
-//        while (iterator.hasNext()) {
-//            System.out.println(iterator.next());
-//        }
-        //By  using Consumer class
-//        MyConsumer action = new MyConsumer();
-//        myList.forEach(action);
-        ////By using anonymous class
-//            myList.forEach(new Consumer<Integer>() {
-//                @Override
-//                public void accept(Integer value) {
-//                    System.out.println(value);
-//                }
-//            });
 
-        //LAMBDA function
-        myList.forEach(value -> {System.out.println(value);});
-//        Consumer<Integer> implementation = value -> System.out.println(value);
-//        myList.forEach(implementation);
+
+        Function<Integer, Double> doubleFunction = (n) -> n.doubleValue();
+        myList.forEach(value -> System.out.println(" Integer Value to Double : " + doubleFunction.apply(value)));
     }
 
 }
